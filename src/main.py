fd5c3c0513c71e7c3f5ff4df64a9179222c88d25
Linda <vfjbrito@gmail.com>
2025-03-24 16:04:05 -0300

    main setup

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import TextNode, TextType
+
+def main():
+    chico =  TextNode('É a que diz bad motherfucker', TextType.LINK, 'https://www.youtube.com/watch?v=NUuwd8Z0l_4')
+    print(chico)
+
+main()
